e043d539f4a1d940b3b7d35b72b4f27f9548a372
kostanichevski <kostanicevski@gmail.com>
2024-02-13 22:59:42 +0100

    Creating the app layout

--- world-wise-app/src/App.jsx
+++ world-wise-app/src/App.jsx
@@ -1,5 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
+
 function App() {
-  return <div>World Wise</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="app" element={<AppLayout />} />
+          <Route path="*" element={<PageNotFound />} />
+          {/* The * is a default route that will be used when none of the routes can be found and display the page not found component in this case */}
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
